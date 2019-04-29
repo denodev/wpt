e085b85fbc6abd524ef00cd486f7ce4fcc047995
迷渡 <justjavac@gmail.com>
2019-04-29 10:22:25 +0800

    add cli `serve` (#1)

--- wpt.ts
+++ wpt.ts
@@ -6,52 +6,58 @@ console.log(`use deno version: ${Deno.version.deno}`);
 
 const decoder = new TextDecoder("utf-8");
 
-switch (Deno.args[1]) {
-  case "serve":
-    Deno.run({
-      args: [
-        "deno",
-        "--allow-net",
-        "https://deno.land/std/http/file_server.ts"
-      ],
-      stdout: "inherit"
-    });
-    break;
-  case "run":
-    const specs = Deno.readDirSync("./spec").filter(
-      x => x.isDirectory() && !ignore.includes(x.name)
-    );
-
-    for (const spec of specs) {
-      console.log(spec.path);
-      const files = Deno.readDirSync(spec.path).filter(
-        x => x.name.substr(-7) === ".any.js"
+async function main() {
+  switch (Deno.args[1]) {
+    case "serve":
+      const p = Deno.run({
+        args: [
+          "deno",
+          "--allow-net",
+          "--allow-read",
+          "https://deno.land/std/http/file_server.ts"
+        ],
+        stdout: "inherit"
+      });
+      await p.status();
+      break;
+    case "run":
+      const specs = Deno.readDirSync("./spec").filter(
+        x => x.isDirectory() && !ignore.includes(x.name)
       );
 
-      for (const file of files) {
-        const content = Deno.readFileSync(file.path);
-        const code: string = decoder.decode(content);
-        setup(`${spec.name}›${file.name}›`);
-        eval(code);
+      for (const spec of specs) {
+        console.log(spec.path);
+        const files = Deno.readDirSync(spec.path).filter(
+          x => x.name.substr(-7) === ".any.js"
+        );
+
+        for (const file of files) {
+          const content = Deno.readFileSync(file.path);
+          const code: string = decoder.decode(content);
+          setup(`${spec.name}›${file.name}›`);
+          eval(code);
+        }
       }
-    }
 
-    runTests(true);
-    break;
-  case "test":
-    if (!Deno.args[2]) {
-      console.log("Please input a file name. Usage: wpt test <filename>");
-      Deno.exit(1);
-    }
+      runTests(true);
+      break;
+    case "test":
+      if (!Deno.args[2]) {
+        console.log("Please input a file name. Usage: wpt test <filename>");
+        Deno.exit(1);
+      }
 
-    const content = Deno.readFileSync(Deno.args[2]);
-    const code: string = decoder.decode(content);
-    eval(code);
-    runTests(false);
-    break;
-  case "build":
-    build();
-    break;
-  default:
-    console.log(Deno.args);
+      const content = Deno.readFileSync(Deno.args[2]);
+      const code: string = decoder.decode(content);
+      eval(code);
+      runTests(false);
+      break;
+    case "build":
+      build();
+      break;
+    default:
+      console.log(Deno.args);
+  }
 }
+
+main();
