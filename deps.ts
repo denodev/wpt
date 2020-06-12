8f3b33f61b79a52de358acfe5b6a57fec454a1ac
迷渡 <justjavac@gmail.com>
2020-06-12 11:37:14 +0800

    bump deno@1.0.1

--- deps.ts
+++ deps.ts
@@ -1,11 +1,11 @@
 // Standard Library
-export { green, red } from "https://deno.land/std@v0.51.0/fmt/colors.ts";
-export { readJson } from "https://deno.land/std@v0.51.0/fs/mod.ts";
+export { green, red } from "https://deno.land/std@v0.52.0/fmt/colors.ts";
+export { readJson } from "https://deno.land/std@v0.52.0/fs/mod.ts";
 export {
   assert,
   AssertionError,
   fail,
-} from "https://deno.land/std@v0.51.0/testing/asserts.ts";
+} from "https://deno.land/std@v0.52.0/testing/asserts.ts";
 
 // Third Party Modules
 export { html } from "https://deno.land/x/html@v1.0.0/mod.ts";
